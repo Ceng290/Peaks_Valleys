35a5ab911e9318c5e1ad741b12c4f04d1e81640a
Ceng290 <Ceng290@hotmail.com>
2017-05-28 15:23:01 -0700

    Add project files.

---
+++ Peaks_Valleys/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Peaks_Valleys.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Aequilibrium | The Castle Company</title>
+    <link href="css/StyleSheet1.css" rel="stylesheet" />
+</head>
+<body>
+  <form id="form1" runat="server">
+    <div>
+      <div>
+        <h1>Aequilibrium | The Castle Company</h1>
+      </div>
+      <br />
+      <div>Please enter the integers that represents the land height.  For example [6,1,4]: <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:RegularExpressionValidator ID="regexpName" runat="server" ErrorMessage="Only enter integers separated by commas." ControlToValidate="TextBox1" ValidationExpression="[0-9]+(,[0-9]+)*" />
+      </div>
+      <br />
+      <div>
+        <asp:Button ID="Button1" runat="server" Text="Submit Values" OnClick="Button1_Click" />
+      </div>
+      <br />
+      <div>
+          The total number of castles to be built are: <asp:Label ID="numCastles" runat="server"></asp:Label>
+      </div>
+    </div>
+  </form>
+</body>
+</html>
+
---
+++ Peaks_Valleys/Default.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Peaks_Valleys
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        //Initialize Global variables
+        int numCastle;
+        string numberCastles;
+
+        //**************************************************************************************
+        //* Page_Load Event
+        //**************************************************************************************
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                
+            }else{
+                //Do something
+            }
+        }
+        //**************************************************************************************
+        //*
+        //**************************************************************************************
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            calculateNumCastles();
+        }
+
+        //**************************************************************************************
+        //*
+        //**************************************************************************************
+        protected void calculateNumCastles()
+        {
+            try
+            {
+                //get the values from the textbox
+                string elevation = TextBox1.Text;
+                string[] myArray = elevation.Split(',');      
+                int[] sequence = new int[myArray.Length];
+
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    sequence[i] = int.Parse(myArray[i].ToString());
+                }
+
+                //delete any consecutive numbers that are equal  IE: [6,1,4,4,4,4,5,5,6,3] ==> [6,1,4,5,6,3]
+                int temp=0;
+                bool flag = false;
+                List<int> termsList = new List<int>();
+                for (int i = 0; i <= sequence.Length - 1; i++) {
+                    if(flag == false){
+                        temp = sequence[i];
+                        termsList.Add(temp);
+                        flag = true;
+                    }
+                    if (sequence[i] != temp) {
+                        temp = sequence[i];
+                        termsList.Add(temp);
+                    }
+                }
+
+                int[] terms = termsList.ToArray();
+
+                //Calculate the number of castles
+                numCastle = getNumberofCastles(terms);
+
+            }
+            catch
+            {
+                //array length = 0 IE: []
+                numCastle = 0;
+            }
+            
+            numberCastles = numCastle.ToString();
+            numCastles.Text = numberCastles;
+        }
+
+        //**************************************************************************************
+        //* Here we determine the number of peaks and valleys from the array sequence
+        //**************************************************************************************
+        int getNumberofCastles(int[] terms)
+        {
+            int numPeaksValleyCastles = 0;
+            //**************************************************************************************
+            //array length = 1 IE: [1]
+            //**************************************************************************************
+            if (terms.Length == 1)
+            {
+                numPeaksValleyCastles = 1;
+            }
+            //**************************************************************************************
+            //* array length = 2 IE: [4,1]
+            //* Making the assumption that if the first number is greater than the second
+            //* the first number is a peak and the second number is a valley
+            //* Therefore 2 castles are needed.
+            //* 
+            //* If the first number == the second number then only 1 castle is needed
+            //**************************************************************************************
+            if (terms.Length == 2){
+                if (terms[0] == terms[1])
+                {
+                    numPeaksValleyCastles = 1;
+                }
+                else
+                {
+                    numPeaksValleyCastles = 2;
+                }
+                
+            }
+            //*******************************************************************************************
+            //*                                                                     V P V       P     
+            //* array length = 3 or more IE: [6,1,4,4,4,1,2,3,4,5,5,5,4,2,1,10] ==> [6,1,4,1,2,3,4,5,4,2,1]
+            //*******************************************************************************************
+            if (terms.Length > 2)
+            {
+
+                for (int j = 1; j <= terms.Length - 1; j++) {
+
+                    if (j < terms.Length - 1) {
+                        if ((terms[j] < terms[j - 1]) && (terms[j] < terms[j + 1])) {
+                            numPeaksValleyCastles = numPeaksValleyCastles + 1;
+                        } else if ((terms[j] > terms[j - 1]) && (terms[j] > terms[j + 1])){
+                            numPeaksValleyCastles = numPeaksValleyCastles + 1;
+                        }
+                    }
+                    
+                }
+                
+            }
+
+            return numPeaksValleyCastles;
+        }
+
+        //**************************************************************************************
+        //* Test Cases
+        //**************************************************************************************        
+    }
+}
